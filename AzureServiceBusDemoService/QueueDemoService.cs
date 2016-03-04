6120a31d9e16ea8ff0eaf181be426179f66dbd07
Benjamin Abt <mail@benjamin-abt.com>
2016-03-04 20:02:50 +0100

    Created sample sender-receiver demo for Azure
    Service Bus

---
+++ AzureServiceBusDemoService/QueueDemoService.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.ServiceBus.Messaging;
+using Newtonsoft.Json;
+
+namespace AzureServiceBusDemoService
+{
+    /// <summary>
+    /// Demo Service
+    /// </summary>
+    public class QueueDemoService : IDisposable
+    {
+
+        private QueueClient _queueClient;
+
+        /// <summary>
+        /// Creates an instance of the demo service
+        /// </summary>
+        /// <param name="connectionString">Your personal connection string from app.config</param>
+        /// <param name="queueName">Name of your Queue</param>
+        public QueueDemoService( string connectionString, string queueName )
+        {
+            // Create client
+            _queueClient = QueueClient.CreateFromConnectionString( connectionString, queueName );
+
+            _queueClient.OnMessage( HandleOnQueueMessage );
+        }
+
+        /// <summary>
+        /// Sends a message to the queue
+        /// </summary>
+        /// <param name="payload">Message to send. Will be converted to json</param>
+        /// <returns>True if message was send, false if queue is closed or diposed.</returns>
+        public bool Send( string payload )
+        {
+            if( _queueClient != null )
+            {
+                _queueClient.Send( new BrokeredMessage( JsonConvert.SerializeObject( payload ) ) );
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Subscripte here to get the messages from queue
+        /// </summary>
+        public EventHandler<string> OnNewMessage;
+
+        /// <summary>
+        /// Handles queue message receive
+        /// </summary>
+        /// <param name="brokeredMessage"></param>
+        private void HandleOnQueueMessage( BrokeredMessage brokeredMessage )
+        {
+            // Get the origin json payload
+            string jsonPayload = brokeredMessage.GetBody<string>();
+
+            // convert to string
+            string message = JsonConvert.DeserializeObject<string>( jsonPayload );
+
+            OnNewMessage?.Invoke( this, message );
+        }
+
+        /// <summary>
+        /// Just a frame to provide using
+        /// </summary>
+        public void Dispose()
+        {
+            if( _queueClient != null )
+            {
+                _queueClient.Close();
+                _queueClient = null;
+            }
+        }
+    }
+}
---
+++ AzureServiceBusSampleReceiver/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Configuration;
+using AzureServiceBusDemoService;
+
+namespace AzureServiceBusSampleReceiver
+{
+    class Program
+    {
+        static void Main( string[ ] args )
+        {
+            string connectionString = ConfigurationManager.AppSettings[ "Microsoft.ServiceBus.ConnectionString" ];
+            string queueName = "schwabencodedemo";
+
+            using( QueueDemoService demoService = new QueueDemoService( connectionString, queueName ) )
+            {
+                // On new message write that to the command line
+                demoService.OnNewMessage += delegate ( object sender, string message )
+                {
+                    Console.WriteLine( "[RECEIVE] " + message );
+                };
+
+                Console.WriteLine( "Listening to queue... press key to exit." );
+                Console.ReadKey();
+            }
+            Console.WriteLine( "You have pressed a key. Queue closed. Enter key to close demo application." );
+            Console.ReadKey();
+
+        }
+    }
+
+
+}
