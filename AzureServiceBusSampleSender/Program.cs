f95df90137a74fa9b8fcb5c90b7453a8208b736f
Benjamin Abt <mail@benjamin-abt.com>
2016-03-05 15:50:10 +0100

    Added sender project

---
+++ AzureServiceBusSampleSender/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Configuration;
+using AzureServiceBusDemoService;
+
+namespace AzureServiceBusSampleSender
+{
+    class Program
+    {
+        static void Main( string[ ] args )
+        {
+
+            string connectionString = ConfigurationManager.AppSettings[ "Microsoft.ServiceBus.ConnectionString" ];
+            string queueName = "schwabencodedemo";
+
+            using( QueueDemoService demoService = new QueueDemoService( connectionString, queueName ) )
+            {
+                // Send text here
+                string send;
+                do
+                {
+                    Console.Write( "Please enter text to send: " );
+                    send = Console.ReadLine();
+
+            
+
+                    Console.Write( "Sending.." );
+                    demoService.Send( send );
+                    Console.WriteLine( "..done" );
+
+                } while( !String.IsNullOrEmpty( send ) );
+
+                Console.WriteLine( "Closing queue.." );
+            }
+            Console.WriteLine( ".. done" );
+
+
+            Console.WriteLine( "Enter key to close demo application." );
+            Console.ReadKey();
+
+        }
+    }
+
+
+}
